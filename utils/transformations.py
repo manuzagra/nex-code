72e0ebbca6760137e2d52eb85248e5ec503ce1d2
manuzagra <manuzagra@gmail.com>
2022-06-17 19:50:41 +0200

    the trasnformations file

--- utils/transformations.py
+++ utils/transformations.py
@@ -22,10 +22,22 @@ def rotation_xyz(x, y, z):
 
 rng = np.random.default_rng(seed=1)
 TRANSFORMATIONS = {0: ((0,0,0), (0,0,0))}
+
 for i in range(1, 100):
-    sigma_angle = np.pi/18 # 10 degrees
-    sigma_dist = 0.1
+    sigma_angle = 1 * np.pi/180 # * (i//2+1)
+    sigma_dist = 0.3 # * (i//2+1)
         
     TRANSFORMATIONS[i] = (tuple(rng.normal(0, sigma_angle, 3)), tuple(rng.normal(0, sigma_dist, 3)))
+
+# for i in range(1, 11):
+#     TRANSFORMATIONS[i] = ((i*np.pi/90, i*np.pi/90, i*np.pi/90), (0, 0, 0))
+
+# for i in range(11, 21):
+#     j = i-10
+#     TRANSFORMATIONS[i] = ((0, 0, 0), (j*0.05, j*0.05, j*0.05))
+
+# for i in range(21, 31):
+#     j = i-20
+#     TRANSFORMATIONS[i] = ((j*np.pi/90, j*np.pi/90, j*np.pi/90), (j*0.05, j*0.05, j*0.05))
 
 TRANSFORMATIONS = {k:{'rotation': rotation_xyz(*v[0]), 'translation': np.array(v[1]).reshape(3,1)} for k,v in TRANSFORMATIONS.items()}
