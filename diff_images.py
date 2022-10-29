ab379b26b4fd73bcb17f360837cb95d326a42ec6
manuzagra <manuzagra@gmail.com>
2022-10-29 12:07:25 +0200

    sabe the values as well

--- diff_images.py
+++ diff_images.py
@@ -1,25 +1,33 @@
+import json
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from utils.image_utils import get_image
 
 
-ground_truth_path = '/mnt/home/users/tic_163_uma/manuzagra/output/exe_data_random-0_2022-10-27_19:38:01/runs/evaluation/scene/rendered_val/images_IMG_0587.png_gt.png'
+ground_truth_path = '/mnt/home/users/tic_163_uma/manuzagra/more_images/images_IMG_0587.png_gt.png'
 images_path = [
-    '/mnt/home/users/tic_163_uma/manuzagra/output/exe_data_random-0_2022-10-27_19:38:01/runs/evaluation/scene/rendered_val/images_IMG_0587.png_ours.png',
-    
+    '/mnt/home/users/tic_163_uma/manuzagra/more_images/av_image_24-5-15-6-14-11-2-22-21_images_IMG_0587.png',
+    '/mnt/home/users/tic_163_uma/manuzagra/more_images/images_IMG_0587.png_ours.png'
 ]
 
 
 def get_name(path):
     return path.split('/')[-1].split('.')[0]
 
-
+values = {}
 for i, path in enumerate(images_path):
     gt = get_image(ground_truth_path)
     img = get_image(path)
 
-    diff = np.sum(np.abs(gt - img), axis=2)/3
+    diff = np.abs(gt - img)
 
-    plt.imsave(f"results/diff_{i}-{path.split('/')[-1]}", diff.astype(np.uint8)[:,:], cmap='gray')
+    values[path] = np.sum(diff)/(diff.size*255)
 
+    diff_img = np.sum(diff, axis=2)/3
+
+    plt.imsave(f"more_images/diff_{i}-{path.split('/')[-1]}", diff_img.astype(np.uint8)[:,:], cmap='gray')
+
+with open(f"more_images/diffs.json", 'w') as f:
+    json.dump(values, f)
